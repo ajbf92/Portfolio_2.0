c15a13f7aae0437ed4fedd97cafa4d8e04c06f05
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-03-13 21:37:09 -0400

    create contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Contact() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value})
+  }
+  // console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
   return (
     <section>
-         <div className='contact' id='contact'>    
-            <h2>Contact Info</h2>
-            <p class="contact-content">Please feel free to reach out by using the email below: </p>
+      <div className="contact" id="contact">
+        <h2>Contact Info</h2>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+          </div>
+          <div>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        {/* <p class="contact-content">Please feel free to reach out by using the email below: </p>
             <address>
             Email: 
             <a href="mailto:placeholder@gmail.com">Placeholder@gmail.com</a>
-            </address>
-        </div>
+            </address> */}
+      </div>
     </section>
   );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,56 +6,56 @@ function Portfolio() {
         <div className='portfolio' id='portfolio'>
             <h2>Projects</h2>
         </div>
-        <div class="main-project">
+        <div className="main-project">
             <a href="https://ajbf92.github.io/run-buddy/">
             <img src={require('../../images/run-buddy.png')} alt="" />
             </a>
         </div>
-        <div class="projects-h3">
-            <h3 class="left">Robot Gladiator Game</h3>
-            <h3 class="right">Password Generator</h3>
+        <div className="projects-h3">
+            <h3 className="left">Robot Gladiator Game</h3>
+            <h3 className="right">Password Generator</h3>
         </div>
-        <div class="projects  row">
-            <div class="col-6">
+        <div className="projects  row">
+            <div className="col-6">
                 <a href="https://ajbf92.github.io/robot-gladiators/">
-                    <img src={require('../../images/robot-gladiator.png')} alt="" class="left" />
+                    <img src={require('../../images/robot-gladiator.png')} alt="" className="left" />
                 </a>
             </div>
-            <div class="col-6">
+            <div className="col-6">
                 <a href="https://ajbf92.github.io/password-generator/" >
-                    <img src={require("../../images/password-generator.png")} alt="" class ="right" />
+                    <img src={require("../../images/password-generator.png")} alt="" className="right" />
                 </a>
             </div>
         </div> 
-        <div class="projects-h3">
-            <h3 class="left">Workday Scheduler</h3>
-            <h3 class="right">Weather Dashboard</h3>
+        <div className="projects-h3">
+            <h3 className="left">Workday Scheduler</h3>
+            <h3 className="right">Weather Dashboard</h3>
         </div>
-        <div class="projects  row">
-            <div class="col-6 exception">
+        <div className="projects  row">
+            <div className="col-6 exception">
                 <a href="https://ajbf92.github.io/work-day-scheduler/">
-                    <img src={require(`../../images/schedule banner.jpeg`)} alt="" class="left" />
+                    <img src={require(`../../images/schedule banner.jpeg`)} alt="" className="left" />
                 </a>
             </div>
-            <div class="col-6 exception">
+            <div className="col-6 exception">
                 <a href="https://ajbf92.github.io/weather-dashboard/" >
-                    <img src={require('../../images/weather banner.jpeg')} alt="" class ="right" />
+                    <img src={require('../../images/weather banner.jpeg')} alt="" className="right" />
                 </a>
             </div>
         </div> 
-        <div class="projects-h3">
-            <h3 class="left">Front-End App Project: Concierge</h3>
-            <h3 class="right">Full stack Project: No Gear Fitness</h3>
+        <div className="projects-h3">
+            <h3 className="left">Front-End App Project: Concierge</h3>
+            <h3 className="right">Full stack Project: No Gear Fitness</h3>
         </div>
-        <div class="projects row">
-            <div class="col-6">
+        <div className="projects row">
+            <div className="col-6">
                 <a href="https://ellacodes2021.github.io/Team-Indecisive-IDK/" >
-                    <img src={require('../../images/concierge-banner.jpg')} alt="" class ="right" />
+                    <img src={require('../../images/concierge-banner.jpg')} alt="" className="right" />
                 </a>
             </div>    
-            <div class="col-6">
+            <div className="col-6">
                 <a href="https://no-gear-fitness.herokuapp.com/" >
-                    <img src={require('../../images/NGF.png')} alt="" class ="right" />
+                    <img src={require('../../images/NGF.png')} alt="" className="right" />
                 </a>
             </div>  
         </div> 
